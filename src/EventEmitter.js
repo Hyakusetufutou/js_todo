70b5d7df3aefbcce21f4e6adfcd3d9cbfa093ae5
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2023-09-23 19:56:13 +0900

    コードを整形

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -1,35 +1,34 @@
 export class EventEmitter {
-    #listeners = new Map();
+  #listeners = new Map();
 
-    addEventListener(type, listener) {
-        if (!this.#listeners.has(type)) {
-            this.#listeners.set(type, new Set());
-        }
-        const listenerSet = this.#listeners.get(type);
-        listenerSet.add(listener);
+  addEventListener(type, listener) {
+    if (!this.#listeners.has(type)) {
+      this.#listeners.set(type, new Set());
+    }
+    const listenerSet = this.#listeners.get(type);
+    listenerSet.add(listener);
+  }
+
+  emit(type) {
+    const listenerSet = this.#listeners.get(type);
+    if (!listenerSet) {
+      return;
     }
 
+    listenerSet.forEach((listener) => {
+      listener.call(this);
+    });
+  }
 
-    emit(type) {
-        const listenerSet = this.#listeners.get(type);
-        if (!listenerSet) {
-            return;
-        }
-
-        listenerSet.forEach(listener => {
-            listener.call(this);
-        });
+  removeEventListener(type, listener) {
+    const listenerSet = this.#listeners.get(type);
+    if (!listenerSet) {
+      return;
     }
-
-    removeEventListener(type, listener) {
-        const listenerSet = this.#listeners.get(type);
-        if (!listenerSet) {
-            return;
-        }
-        listenerSet.forEach(ownListener => {
-            if (ownListener === listener) {
-                listenerSet.delete(listener);
-            }
-        })
-    }
+    listenerSet.forEach((ownListener) => {
+      if (ownListener === listener) {
+        listenerSet.delete(listener);
+      }
+    });
+  }
 }
--- src/view/TodoListView.js
+++ src/view/TodoListView.js
@@ -2,17 +2,20 @@ import { element } from "./html-util.js";
 import { TodoItemView } from "./TodoItemView.js";
 
 export class TodoListView {
-    createElement(todoItems, { onUpdateTodo, onDeleteTodo, onEditTodo }) {
-        const todoListElement   = element`<ul></ul>`;
+  createElement(todoItems, { onUpdateTodo, onDeleteTodo, onEditTodo }) {
+    const todoListElement = element`<ul></ul>`;
 
-        todoItems.forEach(item => {
-            const todoItemView = new TodoItemView();
-            const todoItemElement = !item.isEdit 
-                                    ? todoItemView.createTodoItemElement(item, {onUpdateTodo, onDeleteTodo}) 
-                                    : todoItemView.createTodoItemEditElement(item, {onEditTodo});
-            todoListElement.appendChild(todoItemElement);
-        });
+    todoItems.forEach((item) => {
+      const todoItemView = new TodoItemView();
+      const todoItemElement = !item.isEdit
+        ? todoItemView.createTodoItemElement(item, {
+            onUpdateTodo,
+            onDeleteTodo,
+          })
+        : todoItemView.createTodoItemEditElement(item, { onEditTodo });
+      todoListElement.appendChild(todoItemElement);
+    });
 
-        return todoListElement;
-    }
+    return todoListElement;
+  }
 }
